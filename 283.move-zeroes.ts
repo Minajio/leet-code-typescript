1127180b2ab1e287ad20ff3910b035e275920b35
Minajio <andres.osoriozapata97@gmail.com>
2025-07-21 14:41:09 -0500

    443-string-compression

---
+++ 283.move-zeroes.ts
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=283 lang=typescript
+ *
+ * [283] Move Zeroes
+ */
+
+// @lc code=start
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+function moveZeroes(nums: number[]): void {
+    
+};
+// @lc code=end
+
--- 443.string-compression.ts
+++ 443.string-compression.ts
@@ -7,10 +7,67 @@
 // @lc code=start
 function compress(chars: string[]): number {
 
-  const compressedChars: string[] = [];
+  let finalLength = 0;
+  let current = chars[0]
+  let count = 0;
+  let writingPosition = 0;
+  let lastInsertion = false;
 
-  
-  
+  for(let i = 0; i < chars.length; i++){
+    
+    if(chars[i] !== current || i === chars.length - 1){
+      if(i === chars.length - 1 && chars[i] === current) count++;
+      else if(i === chars.length - 1 && chars[i] !== current){
+        // count++;
+        lastInsertion = true;
+      }
+      chars[writingPosition] = current;
+      writingPosition++;
+      finalLength++;
+      if(count !== 1){
+        if(count > 9){
+          const temp = ''+count;
+          for(let j = 0; j < temp.length; j++){
+            chars[writingPosition] = temp[j];
+            writingPosition++;
+            finalLength++;
+          }
+        }
+        else{
+          chars[writingPosition] = ""+count;
+          writingPosition++;
+          finalLength++;
+        }
+      }
+      count = 0;
+      current = chars[i];
+    }
+    count++;
+  }
+
+  if(lastInsertion){
+    chars[writingPosition] = current;
+      writingPosition++;
+      finalLength++;
+      if(count !== 1){
+        if(count > 9){
+          const temp = ''+count;
+          for(let j = 0; j < temp.length; j++){
+            chars[writingPosition] = temp[j];
+            writingPosition++;
+            finalLength++;
+          }
+        }
+        else{
+          chars[writingPosition] = ""+count;
+          writingPosition++;
+          finalLength++;
+        }
+      }
+  }
+
+  return finalLength;
+
 };
 // @lc code=end
 
